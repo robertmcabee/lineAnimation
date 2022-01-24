c3cdb3cc6edf5f2ef71f5a7e691ee9f9217fb8b2
robertmcabee <82921822+robertmcabee@users.noreply.github.com>
2022-01-23 19:56:44 -0700

    tweak timing

---
+++ script.js
@@ -0,0 +1,16 @@
+const lines = document.querySelectorAll('.line')
+
+function randomizeLines(lines) {
+  console.log('!!!')
+  for (let i = 0; i < lines.length; i++) {
+    if (Math.random() < 0.5) {
+      lines[i].style.rotate = '90deg';
+    } else {
+      lines[i].style.rotate = '0deg';
+    }
+  }
+}
+
+randomizeLines(lines)
+
+setInterval(() => randomizeLines(lines), 10000);
